c1db8f6d862b98394ac50d919c9007c46bc94c58
이준희 <juni416@naver.com>
2022-12-31 19:01:49 +0900

    Security 테스트 패키지 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,12 +21,15 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("com.h2database:h2")
+    runtimeOnly("com.h2database:h2")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // Security 테스트
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<KotlinCompile> {
