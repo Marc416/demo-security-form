40abb20735476ff69c77dc187c8167bdf420c67a
이준희 <juni416@naver.com>
2022-12-31 11:43:54 +0900

    Init project

---
+++ src/main/kotlin/com/example/demospringsecurityform/DemoSpringSecurityFormApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demospringsecurityform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoSpringSecurityFormApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoSpringSecurityFormApplication>(*args)
+}
